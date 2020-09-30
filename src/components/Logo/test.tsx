f7ad0b1ca520575e5512438ca470b33dab37a680
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-09-30 16:56:08 -0300

    Add tests to verify white and black labels

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,21 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should be able to render Logo componente', () => {
-    const { container } = render(<Logo />)
-    const heading = screen.getByRole('heading', { name: /Logo/i })
+  it('should be able to render white label by default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
 
-    expect(heading).toBeInTheDocument()
-    expect(container.firstChild).toMatchSnapshot()
+  it('should be able to render dark label when this color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
