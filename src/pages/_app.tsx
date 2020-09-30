726a827b9d298bef90163093f72596e2ab544bd1
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-09-30 10:43:35 -0300

    Implements ThemeProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>React Avançado - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
@@ -18,7 +20,7 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
